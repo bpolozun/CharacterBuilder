2dbba8c326acd458f4fcb68f9f9eece23bc1a9b3
bpolozun <benjamin@polozune.com>
2017-12-07 21:14:12 -0500

    Added level tables for ranger and bard.

--- Bard.cpp
+++ Bard.cpp
@@ -36,4 +36,246 @@ Bard::~Bard()
 
 void Bard::onLevelUp()
 {
+	switch (level) {
+	case 1:
+		spellsPerDay[0] = 1;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 0;
+		fortSave = 0;
+		reflexSave = 2;
+		willSave = 2;
+		break;
+	case 2:
+		spellsPerDay[0] = 2;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 1;
+		fortSave = 0;
+		reflexSave = 3;
+		willSave = 3;
+		break;
+	case 3:
+		spellsPerDay[0] = 3;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 2;
+		fortSave = 1;
+		reflexSave = 3;
+		willSave = 3;
+		break;
+	case 4:
+		spellsPerDay[0] = 3;
+		spellsPerDay[1] = 1;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 3;
+		fortSave = 1;
+		reflexSave = 4;
+		willSave = 4;
+		break;
+	case 5:
+		spellsPerDay[0] = 4;
+		spellsPerDay[1] = 2;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 3;
+		fortSave = 1;
+		reflexSave = 4;
+		willSave = 4;
+		break;
+	case 6:
+		spellsPerDay[0] = 4;
+		spellsPerDay[1] = 3;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 4;
+		fortSave = 2;
+		willSave = 5;
+		reflexSave = 5;
+		break;
+	case 7:
+		spellsPerDay[0] = 3;
+		spellsPerDay[1] = 3;
+		spellsPerDay[2] = 1;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 5;
+		fortSave = 2;
+		reflexSave = 5;
+		willSave = 5;
+		break;
+	case 8:
+		spellsPerDay[0] = 4;
+		spellsPerDay[1] = 4;
+		spellsPerDay[2] = 2;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 6;
+		fortSave = 2;
+		reflexSave = 5;
+		willSave = 5;
+		break;
+	case 9:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 4;
+		spellsPerDay[2] = 3;
+		spellsPerDay[3] = 0;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 6;
+		fortSave = 3;
+		reflexSave = 6;
+		willSave = 6;
+		break;
+	case 10:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 4;
+		spellsPerDay[2] = 3;
+		spellsPerDay[3] = 1;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 7;
+		fortSave = 3;
+		reflexSave = 7;
+		willSave = 7;
+		break;
+	case 11:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 4;
+		spellsPerDay[3] = 3;
+		spellsPerDay[4] = 0;
+		spellsPerDay[5] = 0;
+		BAB = 8;
+		fortSave = 3;
+		willSave = 7;
+		reflexSave = 7;
+		break;
+	case 12:		
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 4;
+		spellsPerDay[3] = 3;
+		spellsPerDay[4] = 1;
+		spellsPerDay[5] = 0;
+		BAB = 9;
+		fortSave = 4;
+		reflexSave = 8;
+		willSave = 8;
+		break;
+	case 13:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 4;
+		spellsPerDay[3] = 3;
+		spellsPerDay[4] = 1;
+		spellsPerDay[5] = 0;
+		BAB = 9;
+		fortSave = 4;
+		reflexSave = 8;
+		willSave = 8;
+		break;
+	case 14:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 4;
+		spellsPerDay[3] = 4;
+		spellsPerDay[4] = 2;
+		spellsPerDay[5] = 0;
+		BAB = 10;
+		fortSave = 4;
+		reflexSave = 9;
+		willSave = 9;
+		break;
+	case 15:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 5;
+		spellsPerDay[3] = 4;
+		spellsPerDay[4] = 3;
+		spellsPerDay[5] = 0;
+		BAB = 11;
+		fortSave = 5;
+		reflexSave = 9;
+		willSave = 9;
+		break;
+	case 16:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 5;
+		spellsPerDay[3] = 4;
+		spellsPerDay[4] = 3;
+		spellsPerDay[5] = 1;
+		BAB = 12;
+		fortSave = 5;
+		reflexSave = 10;
+		willSave = 10;
+		break;
+	case 17:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 5;
+		spellsPerDay[3] = 4;
+		spellsPerDay[4] = 4;
+		spellsPerDay[5] = 2;
+		BAB = 12;
+		fortSave = 5;
+		reflexSave = 10;
+		willSave = 10;
+		break;
+	case 18:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 5;
+		spellsPerDay[3] = 5;
+		spellsPerDay[4] = 4;
+		spellsPerDay[5] = 3;
+		BAB = 13;
+		fortSave = 6;
+		reflexSave = 11;
+		willSave = 11;
+		break;
+	case 19:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 5;
+		spellsPerDay[3] = 5;
+		spellsPerDay[4] = 5;
+		spellsPerDay[5] = 4;
+		BAB = 14;
+		fortSave = 6;
+		reflexSave = 11;
+		willSave = 11;
+		break;
+	case 20:
+		spellsPerDay[0] = 5;
+		spellsPerDay[1] = 5;
+		spellsPerDay[2] = 5;
+		spellsPerDay[3] = 5;
+		spellsPerDay[4] = 5;
+		spellsPerDay[5] = 5;
+		BAB = 15;
+		fortSave = 6;
+		reflexSave = 12;
+		willSave = 12;
+		break;
+	}
 }
--- Ranger.cpp
+++ Ranger.cpp
@@ -26,4 +26,189 @@ Ranger::Ranger()
 
 Ranger::~Ranger()
 {
+	switch (level) {
+	case 1:
+		spellsPerDay[0] = 0;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 1;
+		fortSave = 2;
+		reflexSave = 2;
+		willSave = 0;
+		break;
+	case 2:
+		spellsPerDay[0] = 0;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 2;
+		fortSave = 3;
+		reflexSave = 3;
+		willSave = 0;
+		break;
+	case 3:
+		spellsPerDay[0] = 0;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 3;
+		fortSave = 3;
+		reflexSave = 3;
+		willSave = 1;
+		break;
+	case 4:
+		spellsPerDay[0] = 0;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 4;
+		fortSave = 4;
+		reflexSave = 4;
+		willSave = 1;
+	case 5:
+		spellsPerDay[0] = 0;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 5;
+		fortSave = 4;
+		reflexSave = 4;
+		willSave = 1;
+	case 6:
+		spellsPerDay[0] = 1;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 6;
+		fortSave = 5;
+		reflexSave = 5;
+		willSave = 2;
+	case 7:
+		spellsPerDay[0] = 1;
+		spellsPerDay[1] = 0;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 7;
+		fortSave = 5;
+		reflexSave = 5;
+		willSave - 2;
+	case 8:
+		spellsPerDay[0] = 1;
+		spellsPerDay[1] = 1;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 8;
+		fortSave = 6;
+		reflexSave = 6;
+		willSave = 2;
+	case 9:
+		spellsPerDay[0] = 2;
+		spellsPerDay[1] = 1;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 9;
+		fortSave = 6;
+		reflexSave = 6;
+		willSave = 3;
+	case 10:
+		spellsPerDay[0] = 2;
+		spellsPerDay[1] = 1;
+		spellsPerDay[2] = 0;
+		spellsPerDay[3] = 0;
+		BAB = 10;
+		fortSave = 7;
+		reflexSave = 7;
+		willSave = 3;
+	case 11:
+		spellsPerDay[0] = 2;
+		spellsPerDay[1] = 1;
+		spellsPerDay[2] = 1;
+		spellsPerDay[3] = 0;
+		BAB = 11;
+		fortSave = 7;
+		reflexSave = 7;
+		willSave = 3;
+	case 12:
+		spellsPerDay[0] = 2;
+		spellsPerDay[1] = 2;
+		spellsPerDay[2] = 1;
+		spellsPerDay[3] = 0;
+		BAB = 12;
+		fortSave = 8;
+		reflexSave = 8;
+		willSave = 8;
+	case 13:
+		spellsPerDay[0] = 3;
+		spellsPerDay[1] = 2;
+		spellsPerDay[2] = 1;
+		spellsPerDay[3] = 0;
+		BAB = 13;
+		fortSave = 8;
+		reflexSave = 8;
+		willSave = 8;
+	case 14:
+		spellsPerDay[0] = 3;
+		spellsPerDay[1] = 2;
+		spellsPerDay[2] = 1;
+		spellsPerDay[3] = 1;
+		BAB = 14;
+		fortSave = 9;
+		reflexSave = 9;
+		willSave = 4;
+	case 15:
+		spellsPerDay[0] = 3;
+		spellsPerDay[1] = 2;
+		spellsPerDay[2] = 2;
+		spellsPerDay[3] = 1;
+		BAB = 15;
+		fortSave = 9;
+		reflexSave = 9;
+		willSave = 5;
+	case 16:
+		spellsPerDay[0] = 3;
+		spellsPerDay[1] = 3;
+		spellsPerDay[2] = 2;
+		spellsPerDay[3] = 1;
+		BAB = 16;
+		fortSave = 10;
+		reflexSave = 10;
+		willSave = 5;
+	case 17:
+		spellsPerDay[0] = 4;
+		spellsPerDay[1] = 3;
+		spellsPerDay[2] = 2;
+		spellsPerDay[3] = 1;
+		BAB = 17;
+		fortSave = 10;
+		reflexSave = 10;
+		willSave = 5;
+	case 18:
+		spellsPerDay[0] = 4;
+		spellsPerDay[1] = 3;
+		spellsPerDay[2] = 3;
+		spellsPerDay[3] = 2;
+		BAB = 18;
+		fortSave = 11;
+		reflexSave = 11;
+		willSave = 6;
+	case 19:
+		spellsPerDay[0] = 4;
+		spellsPerDay[1] = 3;
+		spellsPerDay[2] = 3;
+		spellsPerDay[3] = 3;
+		BAB = 19;
+		fortSave = 11;
+		reflexSave = 11;
+		willSave = 6;
+	case 20:
+		spellsPerDay[0] = 4;
+		spellsPerDay[1] = 4;
+		spellsPerDay[2] = 3;
+		spellsPerDay[3] = 3;
+		BAB = 20;
+		fortSave = 12;
+		reflexSave = 12;
+		willSave = 6;
+	}
 }
